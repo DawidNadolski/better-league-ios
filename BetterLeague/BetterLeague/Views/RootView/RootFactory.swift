412026d36f41d261ba7cd0e233abaab8e081cead
Dawid Nadolski <Dawid@MacBook-Air-admin.local>
2024-12-04 00:33:35 +0100

    Add RootFactory for further use

---
+++ BetterLeague/BetterLeague/Views/RootView/RootFactory.swift
@@ -0,0 +1,12 @@
+//
+//  RootFactory.swift
+//  BetterLeague
+//
+//  Created by Dawid Nadolski on 04/12/2024.
+//
+
+protocol RootFactoryProtocol {
+}
+
+final class RootFactory: RootFactoryProtocol {
+}
