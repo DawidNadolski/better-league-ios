7099e66ec71506cff9cce5a6712f8b21465301e9
Dawid Nadolski <Dawid@MacBook-Air-admin.local>
2024-12-04 00:33:19 +0100

    Change RootViewModel logic to use @Observable

--- BetterLeague/BetterLeague/Views/RootView/RootViewModel.swift
+++ BetterLeague/BetterLeague/Views/RootView/RootViewModel.swift
@@ -5,25 +5,13 @@
 //  Created by Dawid Nadolski on 03/05/2024.
 //
 
-import Apollo
 import Foundation
 
-final class RootViewModel {
+@Observable final class RootViewModel {
     
-    init() { }
+    private let rootFactory: RootFactoryProtocol
     
-    @Published var user: User? = nil
-    
-    func fetchUser() {
-        APIService.shared.apollo.fetch(query: BetterLeagueAPI.GetUserQuery()) { result in
-            switch result {
-            case .success(let graphQLResponse):
-                guard let user = User(responseData: graphQLResponse.data) else {
-                    return
-                }
-            case .failure(let error):
-                return
-            }
-        }
+    init(rootFactory: RootFactoryProtocol = RootFactory()) {
+        self.rootFactory = rootFactory
     }
 }
