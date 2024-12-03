bd1f45776501ed9541ecde1cb3fb4e166479e859
Dawid Nadolski <Dawid@MacBook-Air-admin.local>
2024-12-04 00:43:34 +0100

    Add basic @Observable matches view model

--- BetterLeague/BetterLeague/Views/Matches/MatchesViewModel.swift
+++ BetterLeague/BetterLeague/Views/Matches/MatchesViewModel.swift
@@ -56,110 +56,30 @@ struct UserBetDisplayData: Identifiable {
     }
 }
 
-final class MatchesViewModel: ObservableObject {
+@Observable final class MatchesViewModel: ObservableObject {
     
-    @Published var userBets = [UserBetDisplayData]()
-    @Published var homeTeamGoals: String = ""
-    @Published var awayTeamGoals: String = ""
+    struct Dependencies {
+        let getUserBetsUseCase: () async throws -> [UserBet]
+    }
     
+    var userBets = [UserBet]()
+    var unbetMatchesCount: Int = 0
+    
+    private let dependencies: Dependencies
     private let modelManager = MatchesModelManager()
-    private let displayDataTransformer = UserBetDisplayDataTransformer()
+    
+    init(dependencies: Dependencies) {
+        self.dependencies = dependencies
+    }
     
     func onAppear() {
-        modelManager.fetchModels()
-        setupBindings()
+        fetchData()
     }
     
-    func betButtonTapped(with matchId: String) {
-        let input = BetterLeagueAPI.BetInput(matchId: matchId, homeTeamGoals: Int(homeTeamGoals) ?? 20, awayTeamGoals: Int(awayTeamGoals) ?? 20)
-        APIService.shared.apolloAuthorized.perform(mutation: BetterLeagueAPI.PlaceBetMutation(input: input)) { result in
-            switch result {
-            case .success(let graphQLResponse):
-                print(graphQLResponse)
-                guard let data = graphQLResponse.data else {
-                    print("Something went wrong")
-                    return
-                }
-            case .failure(let error):
-                return
-            }
+    private func fetchData() {
+        Task {
+            userBets = try await dependencies.getUserBetsUseCase()
+            unbetMatchesCount = userBets.filter { $0.isBet }.count
         }
-    }
-    
-    private func setupBindings() {
-        modelManager.userBets
-            .receive(on: DispatchQueue.main)
-            .map(displayDataTransformer.transform(_:))
-            .assign(to: &$userBets)
-    }
-}
-
-struct UserBetDisplayDataTransformer {
-        
-    func transform(_ userBets: [UserBet]) -> [UserBetDisplayData] {
-        userBets
-            .sorted(by: { $0.match.startDate < $1.match.startDate })
-            .map { userBet in
-                guard let bet = userBet.bet else {
-                    return transformUnbetMatch(userBet)
-                }
-                let state = getState(userBet)
-                print(userBet)
-                
-                return UserBetDisplayData(
-                    id: userBet.match.id,
-                    homeTeamName: userBet.match.homeTeam.name,
-                    homeTeamGoals: "\(userBet.match.score.homeTeamGoals)",
-                    betHomeTeamGoals: "\(bet.score.homeTeamGoals)",
-                    awayTeamName: userBet.match.awayTeam.name,
-                    awayTeamGoals: "\(userBet.match.score.awayTeamGoals)",
-                    betAwayTeamGoals: "\(bet.score.awayTeamGoals)",
-                    isBet: userBet.isBet,
-                    state: state
-                )
-            }
-    }
-    
-    private func transformUnbetMatch(_ userBet: UserBet) -> UserBetDisplayData {
-        UserBetDisplayData(
-            id: userBet.match.id,
-            homeTeamName: userBet.match.homeTeam.name,
-            homeTeamGoals: "\(userBet.match.score.homeTeamGoals)",
-            betHomeTeamGoals: "",
-            awayTeamName: userBet.match.awayTeam.name,
-            awayTeamGoals: "\(userBet.match.score.awayTeamGoals)",
-            betAwayTeamGoals: "",
-            isBet: userBet.isBet,
-            state: getState(userBet)
-        )
-    }
-
-    private func getState(_ userBet: UserBet) -> UserBetDisplayData.State {
-        let currentDate = Date()
-        if userBet.match.startDate > currentDate {
-            return .notStarted
-        }
-        
-        let result = getResult(userBet)
-        if userBet.match.startDate < currentDate && !userBet.match.hasEnded {
-            return .live(result)
-        }
-        return .ended(result)
-    }
-    
-    private func getResult(_ userBet: UserBet) -> BetResult {
-        guard let bet = userBet.bet else {
-            return .wrong
-        }
-        
-        let match = userBet.match
-        if (match.score.homeTeamGoals == bet.score.homeTeamGoals) && (match.score.awayTeamGoals == bet.score.awayTeamGoals) {
-            return .exactResult
-        }
-        if (bet.score.result == match.score.result) {
-            return .correctResult
-        }
-        
-        return .wrong
     }
 }
