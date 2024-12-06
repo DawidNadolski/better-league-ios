f51f1a0f29b91f84af20f5e4d6fd497187dfe384
Dawid Nadolski <Dawid@MacBook-Air-admin.local>
2024-12-06 21:18:41 +0100

    Update MatchesView to use list row view

--- BetterLeague/BetterLeague/Views/Matches/MatchesView.swift
+++ BetterLeague/BetterLeague/Views/Matches/MatchesView.swift
@@ -18,44 +18,8 @@ struct MatchesView: View {
     var body: some View {
         VStack(spacing: 16.0) {
             List {
-                ForEach(viewModel.userBets.map(\.match)) { match in
-                    HStack(spacing: 0.0) {
-                        ZStack {
-                            Color.red
-                            Text("FT")
-                                .frame(alignment: .center)
-                        }
-                        .frame(width: 64.0)
-                        
-                        ZStack {
-                            Color.green
-                            VStack(alignment: .center, spacing: 2.0) {
-                                Text("\(match.score.homeTeamGoals)")
-                                    .frame(alignment: .center)
-                                Text("\(match.score.awayTeamGoals)")
-                                    .frame(alignment: .center)
-                            }
-                        }
-                        .frame(width: 32.0)
-                        
-                        ZStack {
-                            Color.black.opacity(0.1)
-                            VStack(alignment: .center, spacing: 2.0) {
-                                HStack {
-                                    Text(match.homeTeam.name)
-                                    Spacer()
-                                }
-                                HStack {
-                                    Text(match.awayTeam.name)
-                                    Spacer()
-                                }
-                            }
-                            .padding(.leading, 8.0)
-                        }
-                    }
-                    .frame(height: 64.0)
-                    .listRowSeparator(.hidden)
-                    .listRowInsets(EdgeInsets())
+                ForEach(viewModel.userBetsDisplayData) { displayData in
+                    MatchListRow(displayData: displayData)
                 }
             }
         }
@@ -65,8 +29,4 @@ struct MatchesView: View {
         .navigationTitle("Matches")
         .navigationBarTitleDisplayMode(.inline)
     }
-}
-
-#Preview {
-    MatchesView(viewModel: .init(dependencies: .init(getUserBetsUseCase: { [] })))
 }
